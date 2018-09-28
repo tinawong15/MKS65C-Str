4665da947ffb350e6b1263e3e74706bbeb362bac
tina <tinaw425@gmail.com>
2018-09-28 00:28:13 -0400

    updated header file

--- str.h
+++ str.h
@@ -1,1 +1,9 @@
-int mystrlen( char * )
+int mystrlen( char * );
+
+char * mystrcpy( char *dest, char *source );
+
+char * mystrncat( char *dest, char *source, int n);
+
+int mystrcmp( char *s1, char *s2 );
+
+char * mystrchr( char *s, char c );
