45b3145baa2b687fd21923915688aacca52da85c
tina <tinaw425@gmail.com>
2018-09-28 15:13:27 -0400

    added strlen

--- str.c
+++ str.c
@@ -1,3 +1,19 @@
 # include "str.h"
-#include <stdio.h>
-#include <string.h>
+# include <stdio.h>
+# include <string.h>
+
+int mystrlen(char *s) {
+  int count = 0;
+  while(*(s++)) {
+    count++;
+  }
+  return count;
+}
+
+int main() {
+    char * str = "hello";
+    printf("Testing strlen with %s\n", str);
+    printf("[standard]: %lu\n", strlen(str));
+    printf("[mine]: %d\n", mystrlen(str));
+    return 0;
+}
