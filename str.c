3361f633e284177ba13b373952bbe7ad22873321
tina <tinaw425@gmail.com>
2018-09-28 15:21:36 -0400

    added tests in main

--- str.c
+++ str.c
@@ -15,5 +15,21 @@ int main() {
     printf("Testing strlen with %s\n", str);
     printf("[standard]: %lu\n", strlen(str));
     printf("[mine]: %d\n", mystrlen(str));
+
+    printf("Testing strcpy with %s\n", str);
+    printf("[standard]:\n");
+    printf("[mine]:\n");
+
+    printf("Testing strncat with %s\n", str);
+    printf("[standard]:\n");
+    printf("[mine]:\n");
+
+    printf("Testing strcmp with %s\n", str);
+    printf("[standard]:\n");
+    printf("[mine]:\n");
+
+    printf("Testing strchr with %s\n", str);
+    printf("[standard]:\n");
+    printf("[mine]:\n");
     return 0;
 }
