a94e92916f1d0f5a5aabc15e20e82fb29f2aaa5d
tina <tinaw425@gmail.com>
2018-09-29 00:19:25 -0400

    added test cases

--- main.c
+++ main.c
@@ -8,24 +8,36 @@ int main() {
     printf("[standard]: %lu\n", strlen(str));
     printf("[mine]: %d\n", mystrlen(str));
 
-    printf("Testing strcpy with %s\n", str);
-    printf("[standard]:\n");
+    char mycpyDest [256] = "hola";
+    char mycpySrc [] = "hi";
+    char cpydest [256] = "hola";
+    char cpysrc [] = "hi";
+    printf("Testing strcpy with %s and %s\n", cpydest, cpysrc);
+    printf("[standard]: %s\n", strcpy(cpydest, cpysrc));
     printf("[mine]:\n");
 
-    char myDest [256] = "goodbye";
-    char mySrc[] = "ciao";
-    char dest [256] = "goodbye";
-    char src [] = "ciao";
-    printf("Testing strncat with %s and %s\n", dest, src);
-    printf("[standard]: %s\n", strncat(dest, src, 2));
-    printf("[mine]: %s\n", mystrncat(myDest, mySrc, 2));
+    char mycatDest [256] = "goodbye";
+    char mycatSrc [] = "ciao";
+    char catdest [256] = "goodbye";
+    char catsrc [] = "ciao";
+    printf("Testing strncat with %s and %s\n", catdest, catsrc);
+    printf("[standard]: %s\n", strncat(catdest, catsrc, 2));
+    printf("[mine]: %s\n", mystrncat(mycatDest, mycatSrc, 2));
 
-    printf("Testing strcmp with %s\n", str);
-    printf("[standard]:\n");
+    char mycmps0 [256] = "hey";
+    char mycmps1 [] = "heyy";
+    char cmps0 [256] = "hey";
+    char cmps1 [] = "heyy";
+    printf("Testing strcmp with %s and %s\n", cmps0, cmps1);
+    printf("[standard]: %d\n", strcmp(cmps0, cmps1));
     printf("[mine]:\n");
 
-    printf("Testing strchr with %s\n", str);
-    printf("[standard]:\n");
-    printf("[mine]:\n");
+    // char mys [256] = "ello";
+    // char myc = 'l';
+    // char s [256] = "ello";
+    // char c [] = "l";
+    // printf("Testing strchr with %s\n", mys, myc);
+    // printf("[standard]: %s\n", strchr(s, c));
+    // printf("[mine]:\n");
     return 0;
 }
